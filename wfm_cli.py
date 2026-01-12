0c0fe0932ada804509f573d5f951cf83d09827fe
Justin Braun <justinbraun335@gmail.com>
2026-01-12 17:11:01 -0500

    feat: implement alt screen logic

--- wfm_cli.py
+++ wfm_cli.py
@@ -2,10 +2,23 @@
 # =                                   WFM_CLI                                    =
 # ================================================================================
 
+import sys
+
 from copy_user_listings import copy_user_listings
 from display_item_listings import display_item_listings
 from display_user_listings import display_user_listings
 from menu import menu
+from utils import clear_screen
+
+
+def enter_alt_screen():
+    sys.stdout.write("\033[?1049h")
+    sys.stdout.flush()
+
+
+def exit_alt_screen():
+    sys.stdout.write("\033[?1049l")
+    sys.stdout.flush()
 
 
 def wfm_cli():
@@ -16,7 +29,15 @@ def wfm_cli():
         display_item_listings(item)
     elif option == "2":
         pass
+    elif option == "7":
+        exit_alt_screen()
 
 
 if __name__ == "__main__":
-    wfm_cli()
+    enter_alt_screen()
+    try:
+        wfm_cli()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        exit_alt_screen()
